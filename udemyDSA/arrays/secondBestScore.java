c414273a278e0227e964a931f93f53e2ac278366
v370r <studyrockers5@gmail.com>
2023-01-11 01:05:34 +0530

    added book solution

--- udemyDSA/arrays/secondBestScore.java
+++ udemyDSA/arrays/secondBestScore.java
@@ -4,16 +4,16 @@ import java.util.Arrays;
 /*
  * order of n return first two biggest numbers from given list
  */
+import java.util.Collections;
 
 public class secondBestScore {
     public static void main(String[] args) {
         int[] a = { 84, 85, 86, 87, 85, 90, 85, 83, 23, 45, 84, 1, 2, 0 };
         System.out.println(Arrays.toString(secondBest(a)));
+        System.out.println(solution(a));
     }
 
     private static Integer[] secondBest(int[] a) {
-        // Step 1: Convert TreeSet to ArrayList or
-        // LinkedList
         Integer[] res = new Integer[2];
         int global_Max = Integer.MIN_VALUE, secondHigh = Integer.MIN_VALUE;
         for (int i = 0; i < a.length; i++) {
@@ -29,6 +29,21 @@ public class secondBestScore {
         res[0] = global_Max;
         res[1] = secondHigh;
         return res;
+
+    }
+
+    private static String solution(int[] a) {
+        int[] arr = a;
+        Arrays.sort(arr);
+        int first = arr[a.length - 1];
+        int second = Integer.MIN_VALUE;
+        for (int i = arr.length - 1; i > 0; i--) {
+            if (arr[i] != first) {
+                second = arr[i];
+                break;
+            }
+        }
+        return first + " " + second;
 
     }
 }
