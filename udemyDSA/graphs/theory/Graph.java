aaaa19ebaefd4d1388e5cdbb2dd55553a18ac33e
v370r <studyrockers5@gmail.com>
2023-03-20 00:57:02 +0530

    added graph nodes

--- udemyDSA/graphs/theory/Graph.java
+++ udemyDSA/graphs/theory/Graph.java
@@ -8,7 +8,7 @@ public class Graph {
 
     public Graph(ArrayList<GraphNode> nodeList) {
         this.nodeList = nodeList;
-        adjacentMatrix = new int[nodeList.size()][nodeList.size() ]
+        adjacentMatrix = new int[nodeList.size()][nodeList.size()];
     }
 
     public void addUndirectedEdge(int i, int j) {
@@ -17,4 +17,53 @@ public class Graph {
 
     }
 
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append("   ");
+        for (int i = 0; i < nodeList.size(); i++) {
+            s.append(nodeList.get(i).name + " ");
+        }
+        s.append("\n");
+        for (int i = 0; i < nodeList.size(); i++) {
+            s.append(nodeList.get(i).name + ": ");
+            for (int j : adjacentMatrix[i]) {
+                s.append((j) + " ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+}
+
+class GraphWithAdjList {
+    ArrayList<GraphNodeWithAdjList> nodeList = new ArrayList<>();
+
+    public GraphWithAdjList(ArrayList<GraphNodeWithAdjList> nodeList) {
+        this.nodeList = nodeList;
+    }
+
+    public void addUndirectedEdge(int i, int j) {
+        GraphNodeWithAdjList first = nodeList.get(i);
+        GraphNodeWithAdjList second = nodeList.get(j);
+        first.neighbours.add(second);
+        second.neighbours.add(first);
+
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (int i = 0; i < nodeList.size(); i++) {
+            s.append(nodeList.get(i).name + ": ");
+            for (int j = 0; j < nodeList.get(i).neighbours.size(); j++) {
+                if (j == nodeList.get(i).neighbours.size() - 1) {
+                    s.append((nodeList.get(i).neighbours.get(j).name));
+                } else {
+                    s.append((nodeList.get(i).neighbours.get(j).name) + " -> ");
+                }
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
 }
--- udemyDSA/graphs/theory/GraphNode.java
+++ udemyDSA/graphs/theory/GraphNode.java
@@ -1,10 +1,24 @@
 package udemyDSA.graphs.theory;
 
+import java.util.ArrayList;
+
 public class GraphNode {
     public String name;
     public int index;
 
     public GraphNode(String name, int index) {
+        this.name = name;
+        this.index = index;
+    }
+}
+
+class GraphNodeWithAdjList {
+    public String name;
+    public int index;
+
+    public ArrayList<GraphNodeWithAdjList> neighbours = new ArrayList<>();
+
+    public GraphNodeWithAdjList(String name, int index) {
         this.name = name;
         this.index = index;
     }
