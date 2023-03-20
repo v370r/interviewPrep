443540bfe9e2d11a048bc89df01b568c6602cca1
v370r <studyrockers5@gmail.com>
2023-03-20 21:11:37 +0530

    added intrnet solution

--- leetCode/floweBed.java
+++ leetCode/floweBed.java
@@ -53,3 +53,22 @@ public class floweBed {
         slide[2] = element;
     }
 }
+
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        if (n == 0) {
+            return true;
+        }
+        for (int i = 0; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0)
+                    && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+                if (n == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
