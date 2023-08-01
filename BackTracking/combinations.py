9174ca321b6f6fa6e2e217d1cdf5045d46125608
v370r <studyrockers5@gmail.com>
2023-08-01 10:57:44 +0530

    added few more questions

---
+++ BackTracking/combinations.py
@@ -0,0 +1,17 @@
+def combinations(n: int, k: int):
+    res = []
+    
+    def backtrack(start, comb):
+        if len(comb) == k:
+            res.append(comb.copy())
+            return 
+        
+        for i in range(start, n+1):
+            comb.append(i)
+            backtrack(i+1, comb)
+            comb.pop()
+    backtrack(1,[])
+    return res
+
+print(combinations(4,2))
+            
